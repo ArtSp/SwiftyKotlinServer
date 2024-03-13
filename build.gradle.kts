312acc77663c168a698abc240eefdc626d782d9f
Artjoms Spole <artjoms.spole@gmail.com>
2024-03-13 12:18:57 +0200

    main; jre build instructions in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,3 +32,18 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
+
+// 1 - Run in terminal to make jar file:  ./gradlew clean jar
+// 2 - In build/libs there will appear compiled file <fileName>.jar.
+// 3 - Copy via ssh to destination: scp <fileName>.jar <userName>@<IP>:/<path>
+// 4 - Run at destination: java -jar <fileName>.jar
+
+tasks.jar {
+    // for the case if inside your project there is some duplicate
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest.attributes["Main-Class"] = "com.civitta.ApplicationKt"
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    archiveBaseName.set(project.name + "-all")
+}
